b7ce01d0716d7e4ef48d007e8cea4e6fac54cf68
ParmarGhanshyam <ghanshyamparmar421@gmail.com>
2022-03-14 15:08:26 +0530

    first model complete in student portion

--- App/semsubject/views.py
+++ App/semsubject/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from .models import Semstudent
+
+from ..systemuser.models import SystermUser
+
 
 # Create your views here.
+def showstudentinfo(request):
+    systemuserdata = SystermUser.objects.get(user_id=request.user)
+    print(systemuserdata.id)
+    semdata = Semstudent.objects.filter(user_id = systemuserdata.id)
+    login_user_name = request.user
+    datalist = []
+    for data in semdata:
+        datalist.append(data)
+    return render(request,'show_student_info.html',{'datalist':datalist,'systemuser':systemuserdata})
+
+
+# def showstudentsubject(request):
+#
